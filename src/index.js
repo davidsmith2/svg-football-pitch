f6718b6d6cc34d6d841f286a80792f42411591b0
David Smith <davidsmith2@gmail.com>
2017-01-27 19:19:34 -0500

    Implement client-side routing for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
+import {useBasename} from 'history';
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import {
-  hashHistory,
+  browserHistory,
   IndexRoute,
   Router,
   Route
 } from 'react-router';
 
-import './index.css';
-import svgFootballPitch from './reducers';
-
-import AppContainer from './containers/appContainer';
 import {Tabs} from './components/tabs';
-
+import AppContainer from './containers/appContainer';
+import svgFootballPitch from './reducers';
+import './index.css';
 
 let store = createStore(svgFootballPitch);
 
 render(
   <Provider store={store}>
-    <Router history={hashHistory}>
+    <Router history={useBasename(() => browserHistory)({basename: process.env.PUBLIC_URL})}>
       <Route path="/" component={AppContainer}>
         <IndexRoute component={Tabs} />
         <Route path=":tab" component={Tabs} />
